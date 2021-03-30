7b8b5c3f5c5aad6e3a431cb085c427624fb64fb8
Norbert Bodziony <norbertbodziony@gmail.com>
2021-03-30 16:10:57 +0200

    fix discord link

--- src/static/links.ts
+++ src/static/links.ts
@@ -2,5 +2,5 @@ export const social = {
   linkedin: 'https://www.linkedin.com/in/norbertbodziony/',
   twitter: 'https://twitter.com/synthetify',
   github: 'https://github.com/Synthetify',
-  discord: 'https://discord.gg/EbUarxDFsh'
+  discord: 'https://discord.gg/EDrf437'
 }
