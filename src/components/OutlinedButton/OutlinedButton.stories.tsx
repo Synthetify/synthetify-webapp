8de19a3de5faf2e4b16b1d70698ad7ad709ce235
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-12 16:37:45 +0200

    Add disable version to storybook

--- src/components/OutlinedButton/OutlinedButton.stories.tsx
+++ src/components/OutlinedButton/OutlinedButton.stories.tsx
@@ -6,4 +6,5 @@ import { action } from '@storybook/addon-actions'
 
 storiesOf('redesign/OutlinedButton', module)
   .addDecorator(withKnobs)
-  .add('black', () => <OutlinedButton name='Mint' onClick={action('clicked')} />)
+  .add('enable', () => <OutlinedButton name='Mint' onClick={action('clicked')} />)
+  .add('disable', () => <OutlinedButton name='Mint' disabled={true} onClick={action('clicked')} />)
