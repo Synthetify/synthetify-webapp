e9080f2ae2b16819c87b7bd4f2be414be8f69388
Mateusz Zając <matzayonc@gmail.com>
2021-06-14 14:26:15 +0200

    theme color comments

--- src/static/theme.ts
+++ src/static/theme.ts
@@ -5,7 +5,8 @@ export const colors = {
     background: '#030313',
     light: '#090B1B',
     kinda: '#1A1A1A',
-    greyish: '#081323'
+    greyish: '#081323',
+    cinder: '#0E0C12' // v2.0 background color
   },
   blue: {
     accent: '#072E5A',
@@ -20,10 +21,10 @@ export const colors = {
   gray: {
     base: '#8DA9C4',
     skeletonBackground: '#8E8B8B',
-    skeletonField: '#C4C4C4',
+    skeletonField: '#C4C4C4', // v2.0 primary text color
     CDCDCD: '#CDCDCD',
-    manatee: '#898B9C',
-    balticSea: 'rgba(40, 36, 46, 0.6)'
+    manatee: '#898B9C', // v2.0 secondary text color
+    balticSea: 'rgba(40, 36, 46, 0.6)' // v2.0 secondary background
   },
   green: {
     main: '#00F9BB',
