7bca654e92428c88a5df41422acb8c8bba309fbc
Mateusz Zając <matzayonc@gmail.com>
2021-06-14 11:33:50 +0200

    enabled action addon for storybook

--- src/components/Navbar/Navbar.stories.tsx
+++ src/components/Navbar/Navbar.stories.tsx
@@ -4,8 +4,10 @@ import NavbarButton from './Button'
 import { action } from '@storybook/addon-actions'
 import { withKnobs } from '@storybook/addon-knobs'
 
-storiesOf('navbar/button', module)
+storiesOf('navbar/buttons', module)
   .addDecorator(withKnobs)
-  .add('button', () => <div style={{ backgroundColor: '#0E0C12', height: '100px', padding: '10px' }}>
-      <NavbarButton name='Staking' onClick={action('clicked')} />
-    </div>)
+  .add('buttons', () => <div style={{ backgroundColor: '#0E0C12', padding: '10px' }}>
+    <NavbarButton name='Staking' onClick={action('clicked')} />
+    <NavbarButton name='Stats' onClick={action('clicked')} />
+    <NavbarButton name='Exchange' onClick={action('clicked')} />
+  </div>)
