0da292fef558e10ab9f19326f3c6befe483de1c4
Norbert Bodziony <norbertbodziony@gmail.com>
2021-06-14 08:14:30 +0200

    move storybook to webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,47 @@
+const path = require('path')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
+const resolve = item => {
+  return path.join(__dirname, '../', item)
+}
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-viewport/register',
+    // '@storybook/preset-create-react-app',
+    // '@storybook/addon-actions/register',
+    // '@storybook/addon-knobs/register',
+    // '@storybook/addon-viewport/register',
     '@storybook/addon-storysource'
   ],
+  core: {
+    builder: 'webpack5'
+  },
   webpackFinal: config => {
-    config.node = {
-      fs: 'empty',
-      tls: 'empty',
-      net: 'empty',
-      module: 'empty',
-      console: true
-    }
-    return require('../config-overrides')(config)
+    // config.node = {
+    //   fs: 'empty',
+    //   tls: 'empty',
+    //   net: 'empty',
+    //   module: 'empty',
+    //   console: true
+    // }
+
+    config.resolve.fallback.crypto = false
+    Object.assign(config.resolve.alias, {
+      '@sb': resolve('.storybook'),
+      '@static': resolve('src/static'),
+      '@components': resolve('src/components'),
+      '@containers': resolve('src/containers'),
+      '@pages': resolve('src/pages'),
+      '@web3': resolve('src/web3'),
+      '@reducers': resolve('src/store/reducers'),
+      '@selectors': resolve('src/store/selectors'),
+      '@sagas': resolve('src/store/sagas'),
+      '@consts': resolve('src/store/consts')
+    })
+    Object.assign(config.resolve.fallback, {
+      fs: false
+    })
+    config.plugins.push(new NodePolyfillPlugin())
+    return config
   }
 }
