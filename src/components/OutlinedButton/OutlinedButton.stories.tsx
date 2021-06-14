4f530f517d564e9d81b4702aeba1f04178271d70
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-14 23:02:01 +0200

    Change OutlinedButton localization in storybook

--- src/components/OutlinedButton/OutlinedButton.stories.tsx
+++ src/components/OutlinedButton/OutlinedButton.stories.tsx
@@ -4,7 +4,7 @@ import { withKnobs } from '@storybook/addon-knobs'
 import { OutlinedButton } from '@components/OutlinedButton/OutlinedButton'
 import { action } from '@storybook/addon-actions'
 
-storiesOf('redesign/OutlinedButton', module)
+storiesOf('buttons/OutlinedButton', module)
   .addDecorator(withKnobs)
   .add('enable', () => <OutlinedButton name='Mint' onClick={action('clicked')} />)
   .add('disable', () => <OutlinedButton name='Mint' disabled={true} onClick={action('clicked')} />)
