ecb8707ba6f07b12fccaf6a24a26f9a73132d035
Mateusz Zając <matzayonc@gmail.com>
2021-06-14 14:15:55 +0200

    style

--- src/components/Cards/style.ts
+++ src/components/Cards/style.ts
@@ -4,14 +4,20 @@ import { colors } from '@static/theme'
 const useStyles = makeStyles((theme: Theme) => ({
   valueCard: {
     backgroundColor: colors.gray.balticSea,
+    borderRadius: 10
   },
   valueCardTitle: {
     color: colors.gray.manatee,
-
+    fontFamily: theme.typography.fontFamily,
+    fontSize: '22px',
+    lineHeight: '26px',
+    fontWeight: 400
   },
   valueCardAmount: {
     color: colors.gray.skeletonField,
-
+    fontFamily: theme.typography.fontFamily,
+    fontSize: '35px',
+    fontWeight: 700
   }
 }))
 
