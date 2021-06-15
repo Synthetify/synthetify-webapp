e8f5a0101484333346fead5a0aee29b5616e9c65
Mateusz Zając <matzayonc@gmail.com>
2021-06-15 10:09:18 +0200

    added label

--- src/components/Input/Inputs.stories.tsx
+++ src/components/Input/Inputs.stories.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import AmountInput from './AmountInput'
+import AmountInputWithLabel from './AmountInputWithLabel'
+
 import { withKnobs } from '@storybook/addon-knobs'
 
 storiesOf('inputs/amount', module)
   .addDecorator(withKnobs)
   .add('default', () => <div style={{ backgroundColor: '#1E1B23', padding: '10px' }}>
     <AmountInput setValue={(value: string) => value } currency={'xUSD'}/>
+  </div>)
+  .add('labeled', () => <div style={{ backgroundColor: '#1E1B23', padding: '30px' }}>
+    <AmountInputWithLabel setValue={(value: string) => value } currency={'xUSD'}/>
   </div>)
--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -10,12 +10,20 @@ const useStyles = makeStyles((theme: Theme) => ({
     paddingRight: 27,
     height: 60,
     fontSize: 22,
-    minWidth: 250
+    minWidth: 250,
+    marginTop: 14
   },
   currency: {
     fontSize: 22,
     color: colors.gray.manatee,
     width: 70
+  },
+  inputLabel: {
+    color: colors.gray.skeletonField,
+    fontSize: 22,
+    lineHeight: '26px',
+    marginBottom: '14px',
+    fontWeight: 600
   }
 }))
 
