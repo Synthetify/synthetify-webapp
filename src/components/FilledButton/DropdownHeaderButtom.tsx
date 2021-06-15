11036b1b46b4c8c4ea504061a1f9e99b24bafae8
Mateusz Zając <matzayonc@gmail.com>
2021-06-15 14:34:43 +0200

    added code to blur by className at dropdown

--- src/components/FilledButton/DropdownHeaderButtom.tsx
+++ src/components/FilledButton/DropdownHeaderButtom.tsx
@@ -3,13 +3,38 @@ import { Button } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import useStyles from './style'
 
+const blurAllOfClass = (className: string) => {
+  const els = document.getElementsByClassName(className)
+
+  const filters = 'blur(4px)'
+
+  for (const i in els) {
+    const el = els[i] as HTMLElement
+    if (!el.style) {
+      return
+    }
+
+    if (el.style.filter !== filters) {
+      el.style.filter = filters
+    } else {
+      el.style.filter = 'none'
+    }
+  }
+}
+
 export interface IProps {
   name: string
+  classToBlur?: string
   disabled?: boolean
+  onClick?: () => void
+  startIcon?: JSX.Element
 }
 export const DropdownHeaderButton: React.FC<IProps> = ({
   name,
-  disabled = false
+  classToBlur = 'blur-at-overlay',
+  disabled = false,
+  startIcon,
+  onClick = () => {}
 }) => {
   const classes = useStyles()
   return (
@@ -19,8 +44,9 @@ export const DropdownHeaderButton: React.FC<IProps> = ({
       classes={{ disabled: classes.disabled }}
       disabled={disabled}
       onClick={() => {
-        console.log('a')
+        blurAllOfClass(classToBlur)
       }}
+      startIcon={startIcon}
       endIcon={<ExpandMoreIcon />}>
       {name}
     </Button>
--- src/components/FilledButton/FilledButton.stories.tsx
+++ src/components/FilledButton/FilledButton.stories.tsx
@@ -9,6 +9,21 @@ import DropdownHeaderButton from './DropdownHeaderButtom'
 storiesOf('buttons/FilledButton', module)
   .addDecorator(withKnobs)
   .add('black', () => <FilledButton name='Mint' onClick={action('clicked')} />)
-  .add('white', () => <FilledButton name='Set to max' onClick={action('clicked')} variant='white'/>)
-  .add('headerDefault', () => <HeaderButton name='Mainnet' onClick={action('clicked')}/>)
-  .add('headerToOverlay', () => <DropdownHeaderButton name='Open Dropdown' />)
+  .add('white', () => (
+    <FilledButton name='Set to max' onClick={action('clicked')} variant='white' />
+  ))
+  .add('headerDefault', () => <HeaderButton name='Mainnet' onClick={action('clicked')} />)
+  .add('headerToOverlay', () => (
+    <div>
+      <DropdownHeaderButton name='Open Dropdown' classToBlur='blur-at-overlay' />
+      <div className='blur-at-overlay'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+        galley of type and scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+        passages, and more recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </div>
+    </div>
+  ))
