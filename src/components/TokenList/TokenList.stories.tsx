a3e3366e7ef2bdb6d8334eb4cc5638431673caa2
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-16 11:47:45 +0200

    Add example TokenList to storybook

---
+++ src/components/TokenList/TokenList.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { TokenList } from '@components/TokenList/TokenList'
+import { IToken } from '@components/TokenItem/TokenItem'
+import { BN } from '@project-serum/anchor'
+
+const xSNY: IToken = {
+  ticker: '$SNY',
+  balance: new BN(562830),
+  decimals: 6,
+  usdValue: 11.6579
+}
+
+const xBTC: IToken = {
+  ticker: '$BTC',
+  balance: new BN(1e6),
+  decimals: 6,
+  usdValue: 39193.3
+}
+
+const SOL: IToken = {
+  ticker: 'SOL',
+  balance: new BN(394987483),
+  decimals: 6,
+  usdValue: 18059.35
+}
+
+const FTT: IToken = {
+  ticker: 'FTT',
+  balance: new BN(2 * 1e6),
+  decimals: 6,
+  usdValue: 34.1
+}
+
+const tokens = [xSNY, xBTC, SOL, FTT]
+
+storiesOf('Tokens/tokenList', module)
+  .addDecorator(withKnobs)
+  .add('Token list example', () => <TokenList tokens={tokens} />)
