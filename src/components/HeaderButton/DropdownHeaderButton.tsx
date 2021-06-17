d1283a3bedf3cc8e38a389d0c1d65c97d4956189
Mateusz Zając <matzayonc>
2021-06-17 10:42:47 +0200

    moved blur logic to separate file and changed to id

--- src/components/HeaderButton/DropdownHeaderButton.tsx
+++ src/components/HeaderButton/DropdownHeaderButton.tsx
@@ -2,41 +2,16 @@ import React from 'react'
 import { Button, Popover, Typography } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import useStyles from './style'
-
-const blurAllOfClass = (className: string) => {
-  const els = document.getElementsByClassName(className)
-
-  for (const i in els) {
-    const el = els[i] as HTMLElement
-    if (!el.style) {
-      return
-    }
-    el.style.filter = 'blur(4px) brightness(0.4)'
-  }
-}
-
-const resetFiltersOfClass = (className: string) => {
-  const els = document.getElementsByClassName(className)
-
-  for (const i in els) {
-    const el = els[i] as HTMLElement
-    if (!el.style) {
-      return
-    }
-    el.style.filter = 'none'
-  }
-}
+import { blurContent, unblurContent } from '@consts/uiUtils'
 
 export interface IProps {
   name: string
-  classToBlur?: string
   disabled?: boolean
   onClick?: () => void
   startIcon?: JSX.Element
 }
 export const DropdownHeaderButton: React.FC<IProps> = ({
   name,
-  classToBlur = 'blur-at-overlay',
   disabled = false,
   startIcon,
   onClick = () => {}
@@ -47,13 +22,13 @@ export const DropdownHeaderButton: React.FC<IProps> = ({
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget)
-    blurAllOfClass(classToBlur)
+    blurContent()
     onClick()
   }
 
   const handleClose = () => {
     setAnchorEl(null)
-    resetFiltersOfClass(classToBlur)
+    unblurContent()
   }
 
   return (
--- src/components/HeaderButton/HeaderButton.stories.tsx
+++ src/components/HeaderButton/HeaderButton.stories.tsx
@@ -4,16 +4,17 @@ import { action } from '@storybook/addon-actions'
 import { withKnobs } from '@storybook/addon-knobs'
 import HeaderButton from './HeaderButton'
 import DropdownHeaderButton from './DropdownHeaderButton'
+import { toBlur } from '@consts/uiUtils'
 
 storiesOf('buttons/HeaderButton', module)
   .addDecorator(withKnobs)
   .add('headerDefault', () => <HeaderButton name='Mainnet' onClick={action('clicked')} />)
   .add('headerToOverlay', () => (
     <div>
-      <DropdownHeaderButton name='Open Dropdown' classToBlur='blur-at-overlay' />
-      <div className='blur-at-overlay'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-
+      <DropdownHeaderButton name='Open Dropdown'/>
+      <br/>
+      <div id={toBlur} style={{ color: '#00F9BB' }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </div>
     </div>
   ))
