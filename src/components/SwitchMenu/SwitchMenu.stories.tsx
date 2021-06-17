f6ff1d6dd23e07eb2c6ae1085665397f2de70ccf
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-18 00:59:49 +0200

    Add background to SwitchMenu storybook

--- src/components/SwitchMenu/SwitchMenu.stories.tsx
+++ src/components/SwitchMenu/SwitchMenu.stories.tsx
@@ -4,13 +4,16 @@ import { storiesOf } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 import SwitchMenu from '@components/SwitchMenu/SwitchMenu'
 import { action } from '@storybook/addon-actions'
+import { colors } from '@static/theme'
 
 storiesOf('menu/switchMenu', module)
   .addDecorator(withKnobs)
   .add('operations', () => (
-    <SwitchMenu
-      items={['Mint', 'Deposit', 'Withdraw', 'Burn', 'Rewards']}
-      maxWidth={800}
-      onChange={action('switch menu')}
-    />
+    <div style={{ backgroundColor: colors.gray.component, padding: '10px' }}>
+      <SwitchMenu
+        items={['Mint', 'Deposit', 'Withdraw', 'Burn', 'Rewards']}
+        maxWidth={800}
+        onChange={action('switch menu')}
+      />
+    </div>
   ))
