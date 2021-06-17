3dfa66913754407ce1ab30e24e9153ecf42630c4
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-17 23:51:33 +0200

    Change theme in MaxButton

--- src/components/CommonButton/CommonButton.stories.tsx
+++ src/components/CommonButton/CommonButton.stories.tsx
@@ -3,10 +3,11 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withKnobs } from '@storybook/addon-knobs'
 import MaxButton from './MaxButton'
+import { colors } from '@static/theme'
 storiesOf('buttons/CommonButton', module)
   .addDecorator(withKnobs)
   .add('maxButton', () => (
-    <div style={{ backgroundColor: '#1E1B23', padding: '10px' }}>
+    <div style={{ backgroundColor: colors.gray.component, padding: '10px' }}>
       <MaxButton onClick={action('clicked')} />
     </div>
   ))
