9ba8ed6bc0243a7c798f51dfac3734394a3112ae
Mateusz Zając <matzayonc>
2021-06-18 13:50:43 +0200

    enabled viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,7 @@ module.exports = {
     // '@storybook/preset-create-react-app',
     '@storybook/addon-actions/register',
     // '@storybook/addon-knobs/register',
-    // '@storybook/addon-viewport/register',
+    '@storybook/addon-viewport/register',
     '@storybook/addon-storysource'
   ],
   core: {
