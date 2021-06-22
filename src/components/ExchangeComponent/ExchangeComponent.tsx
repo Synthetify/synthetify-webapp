a809166def1c41ecfb3950f96dcf44aac2c5572f
Mateusz Zając <matzayonc@gmail.com>
2021-06-22 11:47:13 +0200

    added amount Input

--- src/components/ExchangeComponent/ExchangeComponent.tsx
+++ src/components/ExchangeComponent/ExchangeComponent.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Grid, Typography, Divider } from '@material-ui/core'
 import useStyles from './style'
+import AmountInput from '@components/Input/AmountInput'
 
 export const ExchangeComponent: React.FC<{}> = () => {
   const classes = useStyles()
   return (
-    <Grid container className={classes.root} direction='column' spacing={4}>
+    <Grid container className={classes.root} direction='column' spacing={3}>
       <Grid item>
         <Typography className={classes.title}>Swap</Typography>
         <Divider className={classes.titleDivider} />
       </Grid>
+      <Grid item container>
+        <Grid item>
+          <AmountInput setValue={ () => {} } currency='xUSD'/>
+        </Grid>
+      </Grid>
+
     </Grid>
   )
 }
--- src/components/ExchangeComponent/style.ts
+++ src/components/ExchangeComponent/style.ts
@@ -4,14 +4,18 @@ import { colors } from '@static/theme'
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     background: colors.gray.component,
-    borderRadius: 10
+    borderRadius: 10,
+    padding: 32,
+    paddingTop: 23
   },
   title: {
     fontSize: 22,
-    color: colors.gray.light
+    color: colors.gray.light,
+    lineHeight: '26px'
   },
   titleDivider: {
-    background: colors.gray.light
+    background: colors.gray.light,
+    marginTop: 13
   }
 }))
 
