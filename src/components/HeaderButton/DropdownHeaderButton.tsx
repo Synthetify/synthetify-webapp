178103d7a9aeaf0e632952430d62e76e04b4202c
Mateusz Zając <matzayonc@gmail.com>
2021-06-22 14:51:41 +0200

    added closing on click away

--- src/components/HeaderButton/DropdownHeaderButton.tsx
+++ src/components/HeaderButton/DropdownHeaderButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Popper, Typography } from '@material-ui/core'
+import { Button, Popper, Typography, ClickAwayListener } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import useStyles from './style'
 import { blurContent, unblurContent } from '@consts/uiUtils'
@@ -27,35 +27,40 @@ export const DropdownHeaderButton: React.FC<IProps> = ({
     // could use rewriting to backdrop-filter when browser support is better
     blurContent()
     onClick()
-    setOpen(true)
+    if (open) handleClose()
+    else setOpen(true)
   }
 
   const handleClose = () => {
+    console.log('clck away')
+    if (!open) return
     unblurContent()
     setOpen(false)
   }
 
   return (
-    <div>
-      <Button
-        className={classes.dropdownHeaderButton}
-        variant='contained'
-        classes={{ disabled: classes.disabled }}
-        disabled={disabled}
-        onClick={handleClick}
-        startIcon={startIcon}
-        endIcon={<ExpandMoreIcon />}>
-        <Typography className={classes.dropdownHeaderButtonText}>{name}</Typography>
-      </Button>
-      <Popper open={open} anchorEl={anchorEl} placement='bottom'>
-        <ConnectWallet
-          open={true}
-          options={['phantom', 'sollet', 'extension']}
-          handleClose={() => {}}
-          onSelect={() => {}}
-        />
-      </Popper>
-    </div>
+    <ClickAwayListener onClickAway={handleClose}>
+      <div>
+        <Button
+          className={classes.dropdownHeaderButton}
+          variant='contained'
+          classes={{ disabled: classes.disabled }}
+          disabled={disabled}
+          onClick={handleClick}
+          startIcon={startIcon}
+          endIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.dropdownHeaderButtonText}>{name}</Typography>
+        </Button>
+        <Popper transition open={open} anchorEl={anchorEl} placement='bottom'>
+          <ConnectWallet
+            open={true}
+            options={['phantom', 'sollet', 'extension']}
+            handleClose={() => {}}
+            onSelect={() => {}}
+          />
+        </Popper>
+      </div>
+    </ClickAwayListener>
   )
 }
 export default DropdownHeaderButton
