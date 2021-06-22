6284c8a5ae92a831a55950fd16d6a2815aaed55a
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-22 11:30:43 +0200

    Add className to AmountInput props

--- src/components/Input/AmountInput.tsx
+++ src/components/Input/AmountInput.tsx
@@ -1,20 +1,22 @@
 import { Input, InputAdornment } from '@material-ui/core'
 import React from 'react'
 import useStyles from './style'
+import classNames from 'classnames'
 
 interface IProps {
   setValue: (value: string) => void
   currency: string
   value?: string
   error?: string | null
+  className?: string
 }
 
-export const AmountInput: React.FC<IProps> = ({ setValue, currency, value, error }) => {
+export const AmountInput: React.FC<IProps> = ({ setValue, currency, value, error, className }) => {
   const classes = useStyles()
   return (
     <Input
       error={!!error}
-      className={classes.amountInput}
+      className={classNames(classes.amountInput, className)}
       color='primary'
       type={'text'}
       value={value}
