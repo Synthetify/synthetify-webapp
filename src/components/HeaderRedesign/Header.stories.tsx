d3ab4a15a3abb29188359edc430a754157cd6373
Mateusz Zając <matzayonc@gmail.com>
2021-06-23 16:35:00 +0200

    added blur in stories

--- src/components/HeaderRedesign/Header.stories.tsx
+++ src/components/HeaderRedesign/Header.stories.tsx
@@ -5,36 +5,41 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withKnobs } from '@storybook/addon-knobs'
 import Header from './Header'
+import { toBlur } from '@consts/uiUtils'
 
 storiesOf('ui/HeaderRedesign', module)
   .addDecorator(withKnobs)
   .add('default', () => {
     return (
-      <Header
-        address={DEFAULT_PUBLICKEY}
-        onNetworkSelect={(chosen: string) => {
-          action(`network changed to: ${chosen}`)()
-        }}
-        onWalletSelect={(chosen: string) => {
-          action(`wallet changed to: ${chosen}`)()
-        }}
-        walletConnected={false}
-        landing='staking'
-      />
+      <div id={toBlur}>
+        <Header
+          address={DEFAULT_PUBLICKEY}
+          onNetworkSelect={(chosen: string) => {
+            action(`network changed to: ${chosen}`)()
+          }}
+          onWalletSelect={(chosen: string) => {
+            action(`wallet changed to: ${chosen}`)()
+          }}
+          walletConnected={false}
+          landing='staking'
+        />
+      </div>
     )
   })
   .add('connected', () => {
     return (
-      <Header
-        address={new PublicKey(42)}
-        onNetworkSelect={(chosen: string) => {
-          action(`network changed to: ${chosen}`)()
-        }}
-        onWalletSelect={(chosen: string) => {
-          action(`wallet changed to: ${chosen}`)()
-        }}
-        walletConnected={true}
-        landing='staking'
-      />
+      <div id={toBlur}>
+        <Header
+          address={new PublicKey(42)}
+          onNetworkSelect={(chosen: string) => {
+            action(`network changed to: ${chosen}`)()
+          }}
+          onWalletSelect={(chosen: string) => {
+            action(`wallet changed to: ${chosen}`)()
+          }}
+          walletConnected={true}
+          landing='staking'
+        />
+      </div>
     )
   })
