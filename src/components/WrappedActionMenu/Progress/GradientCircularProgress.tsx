930db6fff735fc9d7bba2ed22f47dfad1760e2eb
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-25 01:37:59 +0200

    Add IProps to GradientCircularProgress

--- src/components/WrappedActionMenu/Progress/GradientCircularProgress.tsx
+++ src/components/WrappedActionMenu/Progress/GradientCircularProgress.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { CircularProgress, makeStyles } from '@material-ui/core'
 
+export interface IProps {
+  firstColor?: string
+  secondColor?: string
+}
+
 const useStyles = makeStyles(() => ({
   circle: {
     stroke: 'url(#linearColors)'
   }
 }))
 
-export const GradientCircularProgress: React.FC = () => {
+export const GradientCircularProgress: React.FC<IProps> = ({
+  firstColor = '#00F9BB',
+  secondColor = '#627EEA'
+}) => {
   const classes = useStyles({})
 
   return (
     <>
       <svg width='0' height='0'>
         <linearGradient id='linearColors' x1='0' y1='0' x2='1' y2='1'>
-          <stop offset='20%' stopColor='#39F' />
-          <stop offset='90%' stopColor='#F3F' />
+          <stop offset='20%' stopColor={firstColor} />
+          <stop offset='90%' stopColor={secondColor} />
         </linearGradient>
       </svg>
       <CircularProgress thickness={4} classes={{ circle: classes.circle }} />
