0f9cf2881999137f5c779e4edae2ec64de7f99a8
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-06-24 12:55:52 +0200

    #22 Add backgrounds to storybook components

--- src/components/WrappedActionMenu/ActionTemplate/ActionTemplate.stories.tsx
+++ src/components/WrappedActionMenu/ActionTemplate/ActionTemplate.stories.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import ActionTemplate from '@components/WrappedActionMenu/ActionTemplate/ActionTemplate'
 import { action } from '@storybook/addon-actions'
+import { colors } from '@static/theme'
 
 storiesOf('WrappedActionMenu/ActionContent', module).add('{template}', () => (
-  <div style={{ maxWidth: 800 }}>
+  <div style={{ maxWidth: 800, backgroundColor: colors.gray.component, padding: '30px' }}>
     <ActionTemplate action='{template}' onClick={action('{template}')} />
   </div>
 ))
--- src/components/WrappedActionMenu/KeyValue/KeyValue.stories.tsx
+++ src/components/WrappedActionMenu/KeyValue/KeyValue.stories.tsx
@@ -2,7 +2,10 @@ import { storiesOf } from '@storybook/react'
 import React from 'react'
 import KeyValue from '@components/WrappedActionMenu/KeyValue/KeyValue'
 import BN from 'bn.js'
+import { colors } from '@static/theme'
 
 storiesOf('WrappedActionMenu/KeyValue', module).add('Available to withdraw', () => (
-  <KeyValue keyName='Available to withdraw' value={new BN(51640189)} decimal={4} unit='xUSD' />
+  <div style={{ backgroundColor: colors.gray.component, padding: '10px' }}>
+    <KeyValue keyName='Available to withdraw' value={new BN(51640189)} decimal={4} unit='xUSD' />
+  </div>
 ))
