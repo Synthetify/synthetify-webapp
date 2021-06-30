3e81941db6f223116430ac8b252d8f1bc411ffee
Adrian <adrianwojciak@interia.pl>
2021-06-30 09:29:33 +0200

    tokens list modal added

--- src/containers/TokenListWrapper/TokenListWrapper.tsx
+++ src/containers/TokenListWrapper/TokenListWrapper.tsx
@@ -1,23 +1,63 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { TokenList } from '@components/TokenList/TokenList'
-import { accountsArray } from '@selectors/solanaWallet'
+import { accountsArray, accounts } from '@selectors/solanaWallet'
+import { assets } from '@selectors/exchange'
+import SelectTokenModal from '@components/Modals/SelectTokenModal/SelectTokenModal'
 import { actions } from '@reducers/modals'
+import { actions as snackbarActions } from '@reducers/snackbars'
+import { blurContent, unblurContent } from '@consts/uiUtils'
 
 export const TokenListWrapper: React.FC = () => {
-  const dispatch = useDispatch()
-  const userTokens = useSelector(accountsArray)
+  const [open, setOpen] = useState(false)
 
-  return <TokenList
-    tokens={userTokens.map(
-      token => ({
-        ...token,
-        ticker: token.symbol ?? 'token',
-        decimals: 6
-      })
-    )}
-    addAccount={() => { dispatch(actions.openModal('createAccount')) }}
-  />
+  const dispatch = useDispatch()
+
+  const userTokens = useSelector(accountsArray)
+  const allTokens = useSelector(assets)
+  const userAccounts = useSelector(accounts)
+
+  return (
+    <>
+      <TokenList
+        tokens={userTokens.map(
+          token => ({
+            ...token,
+            ticker: token.symbol ?? 'token',
+            decimals: 6
+          })
+        )}
+        addAccount={() => {
+          blurContent()
+          setOpen(true)
+        }}
+      />
+      <SelectTokenModal
+        tokens={Object.values(allTokens).map(token => token.symbol)}
+        open={open}
+        centered={true}
+        anchorEl={null}
+        onSelect={(chosen) => {
+          const tokenAddress = Object.values(allTokens).find(token => token.symbol === chosen)?.assetAddress
+          if (userAccounts[chosen]) {
+            dispatch(
+              snackbarActions.add({
+                message: 'Token account already exist.',
+                variant: 'info',
+                persist: false
+              })
+            )
+          } else if (tokenAddress) {
+            dispatch(actions.createAccount({ tokenAddress }))
+          }
+        }}
+        handleClose={() => {
+          unblurContent()
+          setOpen(false)
+        }}
+      />
+    </>
+  )
 }
 
 export default TokenListWrapper
--- src/pages/PagesRouter.tsx
+++ src/pages/PagesRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { toBlur } from '@consts/uiUtils'
 import StakingPage from './StakingPage/StakingPage'
 import Footer from '@components/Footer/Footer'
@@ -13,7 +13,11 @@ export const PagesRouter: React.FC = () => {
         <HeaderRedesignWrapper />
         <Switch>
           <Route path='/staking' component={StakingPage} />
-          <Route path='*' component={StakingPage} />
+          <Route path='*'>
+            <Redirect to='/staking'>
+              <StakingPage />
+            </Redirect>
+          </Route>
         </Switch>
         <Footer />
       </div>
