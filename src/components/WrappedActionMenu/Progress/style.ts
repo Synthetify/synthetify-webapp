5d69a7469b8edab84e9b0bd2ba453ddf0553b8e8
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-07-05 13:20:15 +0200

    hide overflow in Progress

--- src/components/WrappedActionMenu/Progress/style.ts
+++ src/components/WrappedActionMenu/Progress/style.ts
@@ -1,10 +1,16 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, Theme } from '@material-ui/core/styles'
 import { colors } from '@static/theme'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   text: {
     fontSize: 16,
-    color: colors.gray.manatee
+    color: colors.gray.manatee,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 13
+    }
   },
   icon: {
     paddingRight: 10,
