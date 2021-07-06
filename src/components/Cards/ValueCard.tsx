462df3d8a442ffe4d8657042d448fe7c67c56528
Adrian <adrianwojciak@interia.pl>
2021-07-06 12:35:02 +0200

    question mark on mobiles fixed

--- src/components/Cards/ValueCard.tsx
+++ src/components/Cards/ValueCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Card, CardContent, Typography, Divider, Tooltip, Icon } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Card, CardContent, Typography, Divider, Tooltip, Icon, ClickAwayListener, Hidden } from '@material-ui/core'
 import useStyles from './style'
 import HintIcon from '@static/svg/questionMarkCircle.svg'
 
@@ -11,19 +11,43 @@ export interface IProps {
 }
 export const ValueCard: React.FC<IProps> = ({ name, value, hint, onClick }) => {
   const classes = useStyles()
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+
   return (
     <Card className={classes.valueCard} onClick={onClick}>
       <CardContent>
         {hint ? (
-          <Icon>
-            <Tooltip
-              classes={{ tooltip: classes.tooltip, arrow: classes.tooltipArrow }}
-              title={hint}
-              placement='top-end'
-              arrow>
-              <img src={HintIcon} alt='' className={classes.questionMark} />
-            </Tooltip>
-          </Icon>
+          <>
+            <Hidden mdDown>
+              <Icon>
+                <Tooltip
+                  classes={{ tooltip: classes.tooltip, arrow: classes.tooltipArrow }}
+                  title={hint}
+                  placement='top-end'
+                  arrow>
+                  <img src={HintIcon} alt='' className={classes.questionMark} />
+                </Tooltip>
+              </Icon>
+            </Hidden>
+            <Hidden lgUp>
+              <ClickAwayListener onClickAway={() => { setIsPopoverOpen(false) }}>
+                <Icon onClick={() => { setIsPopoverOpen(true) }}>
+                  <Tooltip
+                    classes={{ tooltip: classes.tooltip, arrow: classes.tooltipArrow }}
+                    title={hint}
+                    placement='top-end'
+                    open={isPopoverOpen}
+                    onClose={() => { setIsPopoverOpen(false) }}
+                    disableFocusListener
+                    disableHoverListener
+                    disableTouchListener
+                    arrow>
+                    <img src={HintIcon} alt='' className={classes.questionMark} />
+                  </Tooltip>
+                </Icon>
+              </ClickAwayListener>
+            </Hidden>
+          </>
         ) : null}
         <Typography className={classes.valueCardTitle}>{name}</Typography>
         <Divider className={classes.divider} />
