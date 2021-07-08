068a0b217008f8afad62b6f2a4d918072d59f715
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-07-08 09:49:30 +0200

    Add RewardsTab to storybook

---
+++ src/components/WrappedActionMenu/RewardsTab/RewardsLine/style.ts
@@ -0,0 +1,9 @@
+import { makeStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) => ({
+  text: {
+    fontSize: 22
+  }
+}))
+
+export default useStyles
---
+++ src/components/WrappedActionMenu/RewardsTab/RewardsTab.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import RewardsTab from '@components/WrappedActionMenu/RewardsTab/RewardsTab'
+
+storiesOf('WrappedActionMenu/RewardsTab', module).add('rewards tab', () => <RewardsTab />)
