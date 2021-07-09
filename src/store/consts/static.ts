da23a06a19ee03166a20dbc0644eff36c9f530aa
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-07-09 11:26:42 +0200

    Add default UserStaking data

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -1,4 +1,6 @@
 import { PublicKey } from '@solana/web3.js'
+import { UserStaking } from '@reducers/exchange'
+import { BN } from '@project-serum/anchor'
 
 export const constValue: string = 'constValue'
 
@@ -20,4 +22,11 @@ enum SolanaNetworks {
 const DEFAULT_PUBLICKEY = new PublicKey(0)
 const ORACLE_OFFSET = 6
 const ACCURACY = 6
-export { SolanaNetworks, DEFAULT_PUBLICKEY, ORACLE_OFFSET, ACCURACY }
+const DEFAULT_STAKING_DATA: UserStaking = {
+  amountToClaim: new BN(0),
+  currentRoundPoints: new BN(0),
+  finishedRoundPoints: new BN(0),
+  nextRoundPoints: new BN(0),
+  lastUpdate: new BN(0)
+}
+export { SolanaNetworks, DEFAULT_PUBLICKEY, ORACLE_OFFSET, ACCURACY, DEFAULT_STAKING_DATA }
