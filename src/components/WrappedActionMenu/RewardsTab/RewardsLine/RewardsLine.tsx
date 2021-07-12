46dc9143b6add53ceb09ceccb806265ae4822d28
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-07-12 15:16:43 +0200

    Add MobileTooltip component

--- src/components/WrappedActionMenu/RewardsTab/RewardsLine/RewardsLine.tsx
+++ src/components/WrappedActionMenu/RewardsTab/RewardsLine/RewardsLine.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { ClickAwayListener, Grid, Hidden, Icon, Tooltip, Typography } from '@material-ui/core'
-import HintIcon from '@static/svg/questionMarkCircle.svg'
+import React from 'react'
 import { transformBN } from '@consts/utils'
+import { Grid, Typography } from '@material-ui/core'
 import AnimatedNumber from '@components/AnimatedNumber'
+import MobileTooltip from '@components/MobileTooltip/MobileTooltip'
 import BN from 'bn.js'
 import useStyles from './style'
 
@@ -26,7 +26,6 @@ export const RewardsLine: React.FC<IRewardsLineProps> = ({
   bottomHint
 }) => {
   const classes = useStyles()
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
   const processedHint = (
     <>
       <Typography className={classes.hint}>{hint}</Typography>
@@ -85,32 +84,7 @@ export const RewardsLine: React.FC<IRewardsLineProps> = ({
           {processedBracket}
         </Typography>
       </Grid>
-      <Grid item>
-        <Hidden mdDown>
-          <Icon>
-            <Tooltip classes={{ tooltip: classes.tooltip }} title={processedHint} placement='right'>
-              <img src={HintIcon} alt='' className={classes.questionMark} />
-            </Tooltip>
-          </Icon>
-        </Hidden>
-        <Hidden lgUp>
-          <ClickAwayListener onClickAway={() => setIsPopoverOpen(false)}>
-            <Icon onClick={() => setIsPopoverOpen(true)}>
-              <Tooltip
-                classes={{ tooltip: classes.tooltip }}
-                title={processedHint}
-                placement='bottom'
-                open={isPopoverOpen}
-                onClose={() => setIsPopoverOpen(false)}
-                disableFocusListener
-                disableHoverListener
-                disableTouchListener>
-                <img src={HintIcon} alt='' className={classes.questionMark} />
-              </Tooltip>
-            </Icon>
-          </ClickAwayListener>
-        </Hidden>
-      </Grid>
+      <MobileTooltip hint={processedHint} />
     </Grid>
   )
 }
