66fadbd0623aec041eb31e95764dac10e92b8673
Wojciech Cichocki <cichockiwojciech97@gmail.com>
2021-07-19 21:19:32 +0200

    Remove unused styles from TimeRemainingTooltip

--- src/components/WrappedActionMenu/RewardsTab/TimeRemainingTooltip/style.ts
+++ src/components/WrappedActionMenu/RewardsTab/TimeRemainingTooltip/style.ts
@@ -1,28 +1,12 @@
-import { makeStyles, Theme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import { colors } from '@static/theme'
 
-const useStyles = makeStyles((theme: Theme) => ({
-  text: {
-    fontSize: 22,
-    color: colors.gray.veryLight,
-    lineHeight: 1.5,
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 16
-    }
-  },
-  tooltip: {
-    background: colors.gray.mid,
-    padding: '5px 8px'
-  },
+const useStyles = makeStyles(() => ({
   hint: {
     color: colors.white.main,
     fontWeight: 400,
     fontSize: 13,
     lineHeight: '16px'
-  },
-  questionMark: {
-    height: 17,
-    width: 17
   }
 }))
 
