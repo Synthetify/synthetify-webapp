48d7037e17e81605be2f8b3846b167bbbfda3e5a
Adrian <adrianwojciak@interia.pl>
2021-07-28 09:06:20 +0200

    rewards tooltips on mobile

--- src/components/WrappedActionMenu/RewardsTab/RewardsLine/style.ts
+++ src/components/WrappedActionMenu/RewardsTab/RewardsLine/style.ts
@@ -2,11 +2,17 @@ import { makeStyles, Theme } from '@material-ui/core/styles'
 import { colors } from '@static/theme'
 
 const useStyles = makeStyles((theme: Theme) => ({
+  line: {
+    justifyContent: 'flex-start',
+    [theme.breakpoints.down('xs')]: {
+      justifyContent: 'space-between'
+    }
+  },
   text: {
     fontSize: 22,
     color: colors.navy.lightGrey,
     lineHeight: 1.5,
-    [theme.breakpoints.down('xs')]: {
+    [theme.breakpoints.down('sm')]: {
       fontSize: 16
     }
   },
