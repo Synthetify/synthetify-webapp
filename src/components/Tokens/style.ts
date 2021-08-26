d54d2ea3431e1542361e2dc4401623502c2e9c84
Adrian <adrianwojciak99@gmail.com>
2021-08-26 10:37:13 +0200

    tokens are done

--- src/components/Tokens/style.ts
+++ src/components/Tokens/style.ts
@@ -7,32 +7,12 @@ const useStyles = makeStyles((theme: Theme) => ({
     overflow: 'hidden'
   },
   switch: {
-    marginBottom: 10,
-
-    [theme.breakpoints.down('sm')]: {
-      marginBottom: 6
-    }
-  },
-  header: {
-    height: 66,
-    paddingInline: 16,
-
-    [theme.breakpoints.down('sm')]: {
-      height: 39
-    },
-
-    [theme.breakpoints.down('xs')]: {
-      paddingInline: 11
-    }
+    marginBottom: 8
   },
   title: {
     fontSize: 22,
     fontWeight: 600,
     color: colors.navy.veryLightGrey,
-
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 17
-    },
 
     [theme.breakpoints.down('xs')]: {
       fontSize: 15
@@ -63,18 +43,19 @@ const useStyles = makeStyles((theme: Theme) => ({
   sum: {
     fontSize: 16,
     color: colors.navy.veryLightGrey,
-    fontWeight: 600,
     position: 'absolute',
-    right: 0,
-    minWidth: 159,
+    right: 24,
+    minWidth: 164,
 
     [theme.breakpoints.down('sm')]: {
-      fontSize: 14
+      minWidth: 141,
+      right: 20
     },
 
     [theme.breakpoints.down('xs')]: {
-      minWidth: 95,
-      fontSize: 12
+      minWidth: 85,
+      fontSize: 13,
+      right: 16
     }
   }
 }))
@@ -84,15 +65,17 @@ export const useStylesWithProps = makeStyles<Theme, { current: number }>((theme:
     position: 'relative',
     zIndex: 1,
     overflow: 'hidden',
-    height: 66,
-    paddingInline: 16,
+    height: 69,
+    paddingInline: 24,
 
     [theme.breakpoints.down('sm')]: {
-      height: 39
+      height: 61,
+      paddingInline: 20
     },
 
     [theme.breakpoints.down('xs')]: {
-      paddingInline: 11
+      height: 45,
+      paddingInline: 16
     },
 
     '&::before': {
@@ -103,7 +86,8 @@ export const useStylesWithProps = makeStyles<Theme, { current: number }>((theme:
       width: '200%',
       height: '100%',
       zIndex: -1,
-      background: 'linear-gradient(90deg, #6A6FC5 24.66%, #40BF9F 50%, #40BF9F 78.82%, #6C6BC7 100%)',
+      background: 'linear-gradient(90deg, #655ED4 24.66%, #40BF9F 50%, #40BF9F 78.82%, #655ED4 100%)',
+      opacity: 0.75,
       transform: `translateX(${current === 0 ? 0 : -50}%)`,
       transition: 'transform 300ms'
     }
