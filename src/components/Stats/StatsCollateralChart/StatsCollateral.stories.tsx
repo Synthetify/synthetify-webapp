a586e540e9348a357993c247c1fe1fae0e3e5012
Kamil <cyrekamil@gmail.com>
2021-09-01 13:36:37 +0200

    trying to refesh files

--- src/components/Stats/StatsCollateralChart/StatsCollateral.stories.tsx
+++ src/components/Stats/StatsCollateralChart/StatsCollateral.stories.tsx
@@ -1,6 +1,1 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import StatsCollateralChart from '@components/Stats/StatsCollateralChart/StatsCollateral'
-
-storiesOf('StatsCollateralChart', module)
-  .add('basic', () => <StatsCollateralChart/>)
+test3
