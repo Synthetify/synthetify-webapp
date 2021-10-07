199c201be36c304ba1db24c0b55a377e8e534fb8
Pawel Krawczyk <pawel.krawczyk@interia.com>
2021-10-07 14:21:30 +0200

    add styling

--- src/components/InformationCard/InformationCard.tsx
+++ src/components/InformationCard/InformationCard.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './style'
+import SNYlogo from '@static/svg/synthetifyError.svg'
+import { Grid, Typography, Container, Avatar } from '@material-ui/core'
 const InformationCard = () => {
+  const [display, setDisplay]: boolean = useState(true)
   const classes = useStyles()
   return (
-    <div>
-      <div className={classes.infoBackground}>dsads</div>
-      <div className={classes.info}>
-        <h1 className={classes.text}>TESTOWY TEKST INFORMACYJNY</h1>
-      </div>
+    <div style={{ display: display ? 'block' : 'none' }}>
+      <div className={classes.infoBackground}></div>
+      <Grid className={classes.info}>
+        <Avatar alt='logo Synthetify' src={SNYlogo} className={classes.infoLogo}/>
+        <Container className={classes.infoContainer}>
+          <Typography component='h2' className={classes.infoHeader}>OOPS!</Typography>
+          <Typography component='p' className={classes.infoParagraph}>Something went wrong while loading the page... <br/>Please, try to reload it. If there’ll still be a problem, contact us!</Typography>
+          
+        </Container>
+      </Grid>
     </div>
   )
 }
