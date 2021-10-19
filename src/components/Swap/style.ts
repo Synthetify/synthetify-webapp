525092f45406751d202944b16858b1a2b0ae2283
PawelSzydlo <pawel.szydlo.99@gmail.com>
2021-10-19 08:27:28 +0200

    change typography- exchange

--- src/components/Swap/style.ts
+++ src/components/Swap/style.ts
@@ -128,6 +128,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     ...typography.body2,
     color: colors.navy.grey,
     position: 'relative',
+    lineHeight: '22px',
 
     [theme.breakpoints.down('sm')]: {
       ...typography.subtitle2
@@ -136,14 +137,19 @@ const useStyles = makeStyles((theme: Theme) => ({
   numbersFieldAmount: {
     ...typography.body1,
     color: colors.navy.veryLightGrey,
-
+    lineHeight: '22px',
     [theme.breakpoints.down('sm')]: {
       ...typography.subtitle1
     }
   },
+  numbersFieldGrid: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
   discount: {
     ...typography.body2,
-
+    lineHeight: '22px',
     [theme.breakpoints.down('sm')]: {
       ...typography.subtitle2
     }
@@ -208,7 +214,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     height: 60,
     marginTop: 36,
-    ...importantStyles(typography.body2),
+    ...importantStyles(typography.body1),
 
     '&:hover': {
       backgroundColor: '#4ADFBA !important'
