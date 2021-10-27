5aca4bc72e46e50827bda3087798be31081e3a66
Kamil Cyrek <kamilcyrek@MacBook-Air-Kamil.local>
2021-10-27 14:29:34 +0200

    logic done

--- src/web3/adapters/slope.ts
+++ src/web3/adapters/slope.ts
@@ -1,82 +1,216 @@
-import EventEmitter from 'eventemitter3'
+import {
+  BaseMessageSignerWalletAdapter,
+  pollUntilReady,
+  WalletConnectionError,
+  WalletDisconnectionError,
+  WalletError,
+  WalletNotConnectedError,
+  WalletNotFoundError,
+  WalletPublicKeyError,
+  WalletSignTransactionError
+} from '@solana/wallet-adapter-base'
 import { PublicKey, Transaction } from '@solana/web3.js'
-// import { notify } from '../../utils/notifications'
-import { WalletAdapter } from './types'
-import { DEFAULT_PUBLICKEY } from '@consts/static'
+import bs58 from 'bs58'
 
-type SolflareEvent = 'disconnect' | 'connect'
-type SolflareRequestMethod = 'connect' | 'disconnect' | 'signTransaction' | 'signAllTransactions'
-
-interface SlopeProvider {
-  publicKey?: PublicKey
-  isConnected?: boolean
-  autoApprove?: boolean
-  signTransaction: (transaction: Transaction) => Promise<Transaction>
-  signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>
-  connect: () => Promise<void>
-  disconnect: () => Promise<void>
-  on: (event: SolflareEvent, handler: (args: any) => void) => void
-  request: (method: SolflareRequestMethod, params: any) => Promise<any>
+interface SlopeWallet {
+  connect: () => Promise<{
+    msg: string;
+    data: {
+      publicKey?: string;
+    };
+  }>;
+  disconnect: () => Promise<{ msg: string }>;
+  signTransaction: (message: string) => Promise<{
+    msg: string;
+    data: {
+      publicKey?: string;
+      signature?: string;
+    };
+  }>;
+  signAllTransactions: (messages: string[]) => Promise<{
+    msg: string;
+    data: {
+      publicKey?: string;
+      signatures?: string[];
+    };
+  }>;
+  signMessage: (message: Uint8Array) => Promise<{ data: { signature: string } }>;
 }
 
-export class SlopeWalletAdapter extends EventEmitter implements WalletAdapter {
-  private get _provider(): SolflareProvider | undefined {
-    if ((window as any)?.solflare?.isSolflare) {
-      return (window as any).solflare
-    }
-    return undefined
-  }
+interface SlopeWindow extends Window {
+  Slope?: new () => SlopeWallet;
+}
 
-  constructor() {
+declare const window: SlopeWindow
+
+export interface SlopeWalletAdapterConfig {
+  pollInterval?: number;
+  pollCount?: number;
+}
+
+export class SlopeWalletAdapter extends BaseMessageSignerWalletAdapter {
+  private _connecting: boolean
+  private _wallet: SlopeWallet | null
+  private _publicKey: PublicKey | null
+
+  constructor(config: SlopeWalletAdapterConfig = {}) {
     super()
-    this.connect = this.connect.bind(this)
+    this._connecting = false
+    this._wallet = null
+    this._publicKey = null
+
+    if (!this.ready) pollUntilReady(this, config.pollInterval || 1000, config.pollCount || 3)
   }
 
-  get connected() {
-    return this._provider?.isConnected || false
+  get publicKey(): PublicKey | null {
+    return this._publicKey
   }
 
-  get autoApprove() {
-    return this._provider?.autoApprove || false
+  get ready(): boolean {
+    return typeof window !== 'undefined' && !!window.Slope
+  }
+
+  get connecting(): boolean {
+    return this._connecting
+  }
+
+  get connected(): boolean {
+    return !!this._publicKey
+  }
+
+  async connect(): Promise<void> {
+    try {
+      if (typeof (window.Slope) === 'undefined') {
+        window.open('https://slope.finance/', '_blank')
+      }
+      if (this.connected || this.connecting) return
+      this._connecting = true
+
+      if (!window.Slope) throw new WalletNotFoundError()
+
+      const wallet = new window.Slope()
+
+      let account: string
+      try {
+        const { data } = await wallet.connect()
+
+        if (!data.publicKey) throw new WalletConnectionError()
+
+        account = data.publicKey
+      } catch (error: any) {
+        if (error instanceof WalletError) throw error
+        throw new WalletConnectionError(error?.message, error)
+      }
+
+      let publicKey: PublicKey
+      try {
+        publicKey = new PublicKey(account)
+      } catch (error: any) {
+        throw new WalletPublicKeyError(error?.message, error)
+      }
+
+      this._wallet = wallet
+      this._publicKey = publicKey
+
+      this.emit('connect')
+    } catch (error: any) {
+      this.emit('error', error)
+      throw error
+    } finally {
+      this._connecting = false
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    const wallet = this._wallet
+    if (wallet) {
+      this._wallet = null
+      this._publicKey = null
+
+      try {
+        const { msg } = await wallet.disconnect()
+        if (msg !== 'ok') throw new WalletDisconnectionError(msg)
+      } catch (error: any) {
+        if (!(error instanceof WalletError)) {
+          // eslint-disable-next-line no-ex-assign
+          error = new WalletDisconnectionError(error?.message, error)
+        }
+        this.emit('error', error)
+      }
+    }
+
+    this.emit('disconnect')
+  }
+
+  async signTransaction(transaction: Transaction): Promise<Transaction> {
+    try {
+      const wallet = this._wallet
+      if (!wallet) throw new WalletNotConnectedError()
+
+      try {
+        const message = bs58.encode(transaction.serializeMessage())
+        const { msg, data } = await wallet.signTransaction(message)
+
+        if (!data.publicKey || !data.signature) throw new WalletSignTransactionError(msg)
+
+        const publicKey = new PublicKey(data.publicKey)
+        const signature = bs58.decode(data.signature)
+
+        transaction.addSignature(publicKey, signature)
+        return transaction
+      } catch (error: any) {
+        if (error instanceof WalletError) throw error
+        throw new WalletSignTransactionError(error?.message, error)
+      }
+    } catch (error: any) {
+      this.emit('error', error)
+      throw error
+    }
   }
 
   async signAllTransactions(transactions: Transaction[]): Promise<Transaction[]> {
-    if (!this._provider) {
-      return transactions
+    try {
+      const wallet = this._wallet
+      if (!wallet) throw new WalletNotConnectedError()
+
+      try {
+        const messages = transactions.map((transaction) => bs58.encode(transaction.serializeMessage()))
+        const { msg, data } = await wallet.signAllTransactions(messages)
+
+        const length = transactions.length
+        if (!data.publicKey || data.signatures?.length !== length) throw new WalletSignTransactionError(msg)
+
+        const publicKey = new PublicKey(data.publicKey)
+
+        for (let i = 0; i < length; i++) {
+          transactions[i].addSignature(publicKey, bs58.decode(data.signatures[i]))
+        }
+
+        return transactions
+      } catch (error: any) {
+        if (error instanceof WalletError) throw error
+        throw new WalletSignTransactionError(error?.message, error)
+      }
+    } catch (error: any) {
+      this.emit('error', error)
+      throw error
     }
-
-    return await this._provider.signAllTransactions(transactions)
   }
 
-  get publicKey() {
-    return this._provider?.publicKey || DEFAULT_PUBLICKEY
-  }
+  async signMessage(message: Uint8Array): Promise<Uint8Array> {
+    try {
+      const wallet = this._wallet
+      if (!wallet) throw new WalletNotConnectedError()
 
-  async signTransaction(transaction: Transaction) {
-    if (!this._provider) {
-      return transaction
-    }
-
-    return await this._provider.signTransaction(transaction)
-  }
-
-  connect = async () => {
-    if (!this._provider) {
-      return
-    }
-
-    this._provider?.on('connect', () => {
-      this.emit('connect')
-    })
-
-    return this._provider.connect()
-  }
-
-  disconnect() {
-    if (this._provider) {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      this._provider.disconnect()
-      this.emit('disconnect')
+      try {
+        const response = await wallet.signMessage(message)
+        return bs58.decode(response.data.signature)
+      } catch (error: any) {
+        throw new WalletSignTransactionError(error?.message, error)
+      }
+    } catch (error: any) {
+      this.emit('error', error)
+      throw error
     }
   }
 }
