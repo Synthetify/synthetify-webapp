c103b499f3dadc40c8b5ee7dfb439a0d23b925c3
Pawel Szydlo <pawel.szydlo.99@gmail.com>
2022-01-04 10:47:14 +0100

    add github ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { ViteAliases } from 'vite-aliases'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-
 export default defineConfig({
   plugins: [
     reactRefresh(),
