5fc208a6153d63ebbfb012b84683ecaea3105d34
PawelKrawczyk96 <pawel.krawczyk@interia.com>
2022-05-31 11:30:51 +0200

    change lido week amount

--- src/store/consts/static.ts
+++ src/store/consts/static.ts
@@ -83,7 +83,7 @@ const MARINADE_PER_POINT = 1.43e-7
 const LIDO_PER_POINT = 1.43e-7
 
 const MNDE_WEEK_AMOUNT = 19231
-const LIDO_WEEK_AMOUNT = 2500
+const LIDO_WEEK_AMOUNT = 2257.99
 
 export {
   SolanaNetworks,
