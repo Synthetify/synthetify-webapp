6468cfa174b0c88e8779ceec7c2b606979c7510c
jandutkiewicz0309 <jan.dutkiewicz@op.pl>
2022-09-12 08:06:24 +0100

    change text

--- src/components/WelcomeModal/WelcomeModal.stories.tsx
+++ src/components/WelcomeModal/WelcomeModal.stories.tsx
@@ -2,4 +2,6 @@ import { storiesOf } from '@storybook/react'
 import React from 'react'
 import WelcomeModal from './WelcomeModal'
 
-storiesOf('modal/welcomeModal', module).add('welcomeModal', () => <WelcomeModal />)
+storiesOf('modal/welcomeModal', module).add('welcomeModal', () => (
+  <WelcomeModal open={false} handleClose={function (value: boolean): void {}} />
+))
